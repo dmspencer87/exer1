cde2c2e574c96a766395edb7174fa38d2be1b29d
Daniel Spencer <Daniel.Spencer87@gmail.com>
2017-09-28 22:16:15 -0700

    Implemented multiple simultanous clients

--- EchoServer.java
+++ EchoServer.java
@@ -10,10 +10,7 @@
  *
  *
  ************************************************************************************/
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.PrintStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -22,26 +19,44 @@ public final class EchoServer {
     public static void main(String[] args) throws Exception {
         try (ServerSocket serverSocket = new ServerSocket(22222)) {
             while (true) {
-                try (Socket socket = serverSocket.accept()) {
-                    String address = socket.getInetAddress().getHostAddress();
-                    System.out.printf("Client connected: %s%n", address);
-                    OutputStream os = socket.getOutputStream();
-                    PrintStream out = new PrintStream(os, true, "UTF-8");
-                    out.printf("Hi %s, thanks for connecting!%n", address);
+                try {
+                    Socket socket = serverSocket.accept();
+                    Runnable echo = () -> {
+                        try {
+                            String address = socket.getInetAddress().getHostAddress();
+                            System.out.printf("Client connected: %s%n", address);
+                            OutputStream os = socket.getOutputStream();
+                            PrintStream out = new PrintStream(os, true, "UTF-8");
+                            out.printf("Hi %s, thanks for connecting!%n", address);
 
-                    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                    while(true){
-                        String str=in.readLine();
-                        if(str.equals("exit")){
-                            System.out.printf("Client disconnected: %n", address);
-                            socket.close();
-                            break;
+                            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                            while (true) {
+                                String str = in.readLine();
+                                if (str.equals("exit")) {
+                                    System.out.printf("Client disconnected: %n", address);
+                                    socket.close();
+                                    break;
+                                }
+                                out.println(str);
+                            }
                         }
-                        out.println(str);
-                    }
+                        catch(InterruptedIOException e){
+                            return;
+                        }
+                        catch(IOException e){
+                            return;
+                        }
+                    };
+
+                    Thread echoThread = new Thread(echo);
+                    echoThread.start();
+
                 }
+                catch(Exception e){
+                    return;
+                }
+
             }
         }
     }
 }
-
